d06b9c4b40208f57ecf5525780911967c73d88f9
Srivalli-reddy <reddysrivalli8764@gmail.com>
2025-04-10 13:01:38 +0530

    Add files via upload

---
+++ project/Customer.java
@@ -0,0 +1,18 @@
+package project;
+
+public class Customer {
+    private String name;
+   
+
+    public Customer(String name, String phone) {
+        this.name = name;
+        
+    }
+
+    public String getName() { return name; }
+   
+}
+ 
+
+
+
